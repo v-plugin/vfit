e28f626cf946a9260749f7080d4321725b9991a4
douyabo <33983165+v-plugin@users.noreply.github.com>
2026-01-27 22:03:27 +0800

    chore(docs): 移除网站迁移公告组件
    公告期已过，不再需要显示迁移通知。删除相关组件以简化代码结构。

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,13 +1,6 @@
 import DefaultTheme from 'vitepress/theme'
-import { h } from 'vue'
-import Announcement from './components/Announcement.vue'
 import './style.css'
 
 export default {
-  ...DefaultTheme,
-  Layout() {
-    return h(DefaultTheme.Layout, null, {
-      'layout-bottom': () => h(Announcement)
-    })
-  }
+  ...DefaultTheme
 }
